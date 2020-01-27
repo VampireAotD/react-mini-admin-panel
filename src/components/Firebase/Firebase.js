d68e82f4c9c8775dc121cc145eb1337d85a155b2
VampireAotD <vampireeeeeeeee@mail.ru>
2020-01-27 12:04:58 +0300

    Initial commit

--- src/components/Firebase/Firebase.js
+++ src/components/Firebase/Firebase.js
@@ -2,13 +2,7 @@ import firebase from 'firebase/app'
 import 'firebase/storage'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCmQmRNNGLHGdmKPXMu6WMEXpXI2erUaZQ",
-    authDomain: "react-mini-shop.firebaseapp.com",
-    databaseURL: "https://react-mini-shop.firebaseio.com",
-    projectId: "react-mini-shop",
-    storageBucket: "react-mini-shop.appspot.com",
-    messagingSenderId: "71414383773",
-    appId: "1:71414383773:web:8511fc4eef8905548d4074"
+
 };
 
 export default firebase.initializeApp(firebaseConfig)
